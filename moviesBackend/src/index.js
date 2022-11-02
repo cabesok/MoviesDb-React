19bbeb0428fba1c50a76e23f600cddc46f87e60e
Emiliano Costa <emiliano@MacBook-Air-de-Emiliano.local>
2022-11-02 20:24:51 -0300

    upload deploy

--- moviesBackend/src/index.js
+++ moviesBackend/src/index.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const path = require("path");
-//const methodOverride = require('method-override');
 const app = express();
-//const session = require("express-session");
-//const userLoggedMiddleware = require("./middlewares/userLoggedMiddleware");
-const cors = require("cors")
-//const bodyParser = require('body-parser');
+const path = require("path");
+const cors = require("cors");
+require('dotenv').config();
+
 
 /* if (process.env.NODE_ENV !== 'production'){
     require('dotenv').config();
@@ -18,9 +16,6 @@ app.listen(app.get("port"),() => console.log("Server Start http://localhost:" + 
 // App Access Public
 app.use(express.static(path.resolve(__dirname, "../public")));
 
-// App Settings (View Engine)
-//app.set("view engine", "ejs");
-//app.set("views", path.resolve(__dirname, "./views"));
 
 // App Middlewares
 app.use(express.urlencoded({extended:false}));
@@ -30,35 +25,10 @@ app.use(express.json());
 
 //allow OPTIONS on all resources
 app.options('*', cors())
-
 app.use(cors());
-//app.use(session({secret: "Secreto", resave: false, saveUninitialized: false}));
-//app.use(userLoggedMiddleware);
-
-
-
 
 
 // App Routes
 
 const movies = require("./routes/moviesApi");
 app.use(movies);    
-
-/* const emisiones = require("./routes/emisiones");
-app.use(emisiones);
-
-const user = require("./routes/user");
-app.use(user); */
-
-/* const home = require("./routes/home");
-app.use(home); */
-
-
-
-// API routes
-
-/* const productApi = require("./routes/api/productApi");
-app.use("/productApi", productApi);
-
-const userApi = require("./routes/api/userApi");
-app.use("/userApi", userApi); */
